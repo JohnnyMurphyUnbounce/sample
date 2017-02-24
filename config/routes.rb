ad9bb9f877b0fe0fca501843fc634f9692e88c9c
JohnnyMurphyUnbounce <johnnymurphy@johnny-murphy.local>
2017-02-24 10:41:01 -0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  	root 'application#hello'
-  end
+  	get 'static_pages/home'
+  	get  'static_pages/help'
+  	get  '/about',   to: 'static_pages#about'
+  	get  '/contact', to: 'static_pages#contact'
+ 	root 'static_pages#home'
+ 	get  '/signup',  to: 'users#new'
+ 	resources :users
 end
